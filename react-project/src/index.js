3858caffa0e4f76578970fb1b5671e15135facac
julia-parewick <124746954+julia-parewick@users.noreply.github.com>
2023-02-14 21:13:12 +0000

    Landing page and table

---
+++ react-project/src/index.js
@@ -0,0 +1,7 @@
+import React, { createElement } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// react component
+import App from "./App";
+
+ReactDOM.render(<App library="React"/>,document.getElementById("root"));
