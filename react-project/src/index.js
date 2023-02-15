421a5c1493100ae6dcefd44ab173539c89713dae
julia-parewick <124746954+julia-parewick@users.noreply.github.com>
2023-02-15 19:29:31 +0000

    Routing

--- react-project/src/index.js
+++ react-project/src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // react component
 import App from "./movies";
-import App2 from "./submit";
+import {BrowserRouter} from 'react-router-dom';
+
 
 // ReactDOM.render(<App library="React"/>,document.getElementById("root"));
 // ReactDOM.render(<App2 library="React"/>,document.getElementById("root2"))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <BrowserRouter>
         <App />
-    </React.StrictMode>
+    </BrowserRouter>
 )
