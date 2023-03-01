7acca6137699854ad1624711f6b180ebda17e041
julia-parewick <124746954+julia-parewick@users.noreply.github.com>
2023-03-01 04:38:44 +0000

    mongodb connection and add/remove functionality

--- react-project/backend/src/server.js
+++ react-project/backend/src/server.js
@@ -1,41 +1,59 @@
 import express from 'express';
+import fs from 'fs';
+import {MongoClient} from 'mongodb';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-let moviesData = 
-        [
-            {"title":"Harry Potter and the Philosopher Stone", 
-            "releaseDate": "01-10-1989", 
-            "actors": ["Daniel Radcliffe","Rupert Grint","Emma Watson"], 
-            "img": "https://m.media-amazon.com/images/M/MV5BNmQ0ODBhMjUtNDRhOC00MGQzLTk5MTAtZDliODg5NmU5MjZhXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_.jpg",
-            "rating":5},
+let moviesData = JSON.parse(fs.readFileSync('movies.json')); 
 
-            {"title":"Harry Potter and the Chamber of Secrets", 
-            "releaseDate": "03-09-1992", 
-            "actors": ["Daniel Radcliffe","Rupert Grint","Emma Watson"], 
-            "img": "https://m.media-amazon.com/images/M/MV5BMjE0YjUzNDUtMjc5OS00MTU3LTgxMmUtODhkOThkMzdjNWI4XkEyXkFqcGdeQXVyMTA3MzQ4MTc0._V1_.jpg", 
-            "rating":5}
-        ];
+const saveData = () => {
+    const jsonContent = JSON.stringify(moviesData);
+    fs.writeFile('./movies.json',jsonContent,'utf8',function(err){
+        if(err){
+            console.log('An error occurred while writing')
+        }
+        else{
+            console.log('File has been saved');
+        }
+    })
+}
+
+app.get('/movies', async (req, res) => {
+    
+    const client = new MongoClient('mongodb://127.0.0.1:27017/');
+    await client.connect();
+
+    const db = client.db('moviesDB');
+
+    const movieData = await db.collection('moviecollection').find({}).toArray();
+    res.json( movieData );
+});
 
 app.get('/',(req,res)=>{
     res.json(moviesData);
 })
 
-app.post('/', (req,res)=>{
-    moviesData = moviesData.filter(function(val,i,arr){return i!=req.body.index})
-    console.log(moviesData)
+app.post('/', async (req,res)=>{
+    // moviesData = moviesData.filter(function(val,i,arr){return i!=req.body.index})
+    // saveData();
+    const data = req.body.data.split(',');
+    const client = new MongoClient('mongodb://127.0.0.1:27017/');
+    await client.connect();
+    const db = client.db('moviesDB');
+    const deleteOperation = await db.collection('moviecollection').deleteOne({'title':data[0],'releaseDate':data[1],'actors':data[2],'img':data[3],'rating':data[4]})
     res.redirect('/');
 })
 
-app.get('/movies',(req,res)=>{
-    res.json(moviesData);
-})
+app.post('/submit', async (req,res)=>{
+    // moviesData.push({"title": req.body.title,"releaseDate":req.body.releaseDate,"actors":req.body.actors.split(','),"img":req.body.img,"rating":req.body.rating});
+    // saveData();
 
-
-app.post('/submit', (req,res)=>{
-    moviesData.push({"title": req.body.title,"releaseDate":req.body.releaseDate,"actors":req.body.actors.split(','),"img":req.body.img,"rating":req.body.rating});
+    const client = new MongoClient('mongodb://127.0.0.1:27017/');
+    await client.connect();
+    const db = client.db('moviesDB');
+    const insertOperation = await db.collection('moviecollection').insertOne({"title": req.body.title,"releaseDate":req.body.releaseDate,"actors":req.body.actors.split(','),"img":req.body.img,"rating":req.body.rating});
     res.redirect('/');
 })
 
